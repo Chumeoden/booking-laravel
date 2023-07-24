47c8faf2a057fb1d6930ed9f6d12cc6291656a56
NEKOshirp <124141538+NEKOshirp@users.noreply.github.com>
2023-07-24 12:17:45 +0700

    add container

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,107 +1,268 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Trang chủ</title>
-    <style>
-        .user-avatar {
-            width: 40px;
-            height: 40px;
-            border-radius: 50%;
-            background-color: #ccc;
-            display: inline-block;
-            cursor: pointer;
-        }
-
-        .user-menu {
-            display: none;
-            position: absolute;
-            top: 60px; /* Để bảng nhỏ hiển thị bên dưới avatar */
-            right: 0;
-            background-color: #f9f9f9;
-            padding: 8px;
-            border: 1px solid #ccc;
-        }
-
-        /* Thêm CSS để đảo ngược vị trí của phần hiển thị khi đăng nhập */
-        .user-info {
-            float: right;
-            margin-right: 20px;
-        }
-
-        .nav-links {
-            float: right;
-        }
-
-        /* Đặt kích thước của logo thành 200px */
-        .logo {
-            width: 200px;
-        }
-
-        /* CSS cho các nút đăng nhập và đăng ký */
-        .auth-button {
-            padding: 10px 20px;
-            background-color: #fff;
-            color: #00f;
-            border: 2px solid #00f;
-            border-radius: 5px;
-            text-decoration: none;
-            margin-left: 10px;
-        }
-
-        /* CSS cho màu chữ màu xanh */
-        .auth-button:hover {
-            background-color: #00f;
-            color: #fff;
-        }
-    </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css'>
+    <link rel='stylesheet' href='https://fonts.googleapis.com/icon?family=Material+Icons'>
+    <title>Document</title>
 </head>
 <body>
-    <!-- Hiển thị logo với kích thước 200px -->
-    <img src="{{ asset('images/logo-booking-login.png') }}" alt="Booking.com Logo" class="logo">
+    <script>
+        .slider .btn {
+  margin-top: 20px;
+}
+
+.section-search input {
+  padding: 5px !important;
+  font-size: 18px !important;
+  width: 90% !important;
+  border: #f4f4f4 3px solid !important;
+}
+
+.section-follow .fa-4x {
+  margin: 5px 10px;
+}
+
+.section-contact h5 {
+  margin-bottom: 30px;
+}
+
+    </script>
     
-    @if(session('success'))
-    <div>
-        <p style="color: green;">{{ session('success') }}</p>
-    </div>
-    @endif
-
-    <!-- Hiển thị nút đăng nhập và đăng ký nếu người dùng chưa đăng nhập -->
-    @guest
-        <!-- Đảo ngược vị trí của các nút đăng nhập và đăng ký -->
-        <div class="nav-links">
-            <a href="/login" class="auth-button">Đăng nhập</a>
-            <a href="/register" class="auth-button">Đăng ký</a>
-        </div>
-    @else
-        <!-- Hiển thị avatar người dùng và thông tin người dùng -->
-        <div class="user-info">
-            <div class="user-avatar" id="userAvatar"></div>
-            
-            <!-- Hiển thị bảng nhỏ khi nhấp vào avatar -->
-            <div class="user-menu" id="userMenu">
-                <ul><span>{{ Auth::user()->name }}</span></ul>
-                <ul><a href="/user-profile">Xem thông tin tài khoản</a></ul>
-                <a href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="auth-button">Đăng xuất</a>
-                <form id="logout-form" action="{{ route('logout') }}" method="post" style="display: none;">
-                    @csrf
-                </form>
+    
+      
+    
+      <!-- Section: Search -->
+      <section id="search" class="section section-search teal darken-1 white-text center scrollspy">
+        <div class="container">
+          <div class="row">
+            <div class="col s12">
+              <h3>Search Destinations</h3>
+              <div class="input-field">
+                <input class="white grey-text autocomplete" placeholder="Aruba, Cancun, etc..." type="text" id="autocomplete-input">
+              </div>
             </div>
-
-            <!-- Script để xử lý sự kiện click vào avatar -->
-            <script>
-                var avatar = document.getElementById('userAvatar');
-                var menu = document.getElementById('userMenu');
-
-                avatar.addEventListener('click', function() {
-                    if (menu.style.display === 'block') {
-                        menu.style.display = 'none';
-                    } else {
-                        menu.style.display = 'block';
-                    }
-                });
-            </script>
+          </div>
         </div>
-    @endguest
-
+      </section>
+    
+      <!-- Section: Icon Boxes -->
+      <section class="section section-icons grey lighten-4 center">
+        <div class="container">
+          <div class="row">
+            <div class="col s12 m4">
+              <div class="card-panel">
+                <i class="material-icons large teal-text">room</i>
+                <h4>Pick Where</h4>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem, velit.</p>
+              </div>
+            </div>
+            <div class="col s12 m4">
+              <div class="card-panel">
+                <i class="material-icons large teal-text">store</i>
+                <h4>Travel Shop</h4>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem, velit.</p>
+              </div>
+            </div>
+            <div class="col s12 m4">
+              <div class="card-panel">
+                <i class="material-icons large teal-text">airplanemode_active</i>
+                <h4>Fly Cheap</h4>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem, velit.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    
+      <!-- Section: Popular -->
+      <section id="popular" class="section section-popular scrollspy">
+        <div class="container">
+          <div class="row">
+            <h4 class="center">
+              <span class="teal-text">Popular</span> Places</h4>
+            <div class="col s12 m4">
+              <div class="card">
+                <div class="card-image">
+                  <img src="https://image.ibb.co/hbEMux/resort1.jpg" alt="">
+                  <span class="card-title">Cancun, Mexico</span>
+                </div>
+                <div class="card-content">
+                  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas aliquid fugiat corporis laudantium, architecto
+                    delectus?
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col s12 m4">
+              <div class="card">
+                <div class="card-image">
+                  <img src="https://image.ibb.co/mn1egc/resort2.jpg" alt="">
+                  <span class="card-title">The Bahamas</span>
+                </div>
+                <div class="card-content">
+                  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas aliquid fugiat corporis laudantium, architecto
+                    delectus?
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col s12 m4">
+              <div class="card">
+                <div class="card-image">
+                  <img src="https://image.ibb.co/mbCVnH/resort3.jpg" alt="">
+                  <span class="card-title">Nova Scotia</span>
+                </div>
+                <div class="card-content">
+                  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas aliquid fugiat corporis laudantium, architecto
+                    delectus?
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col s12 center">
+              <a href="#contact" class="btn btn-large grey darken-3">
+                <i class="material-icons left">send</i> Contact For Booking
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    
+      <!-- Section: Follow -->
+      <section class="section section-follow teal darken-2 white-text center">
+        <div class="container">
+          <div class="row">
+            <div class="col s12">
+              <h4>Follow Travelville</h4>
+              <p>Follow us on social media for special offers</p>
+              <a href="htttps://facebook.com" target="_blank" class="white-text">
+                <i class="fab fa-facebook fa-4x"></i>
+              </a>
+              <a href="htttps://twitter.com" target="_blank" class="white-text">
+                <i class="fab fa-twitter fa-4x"></i>
+              </a>
+              <a href="htttps://linkedin.com" target="_blank" class="white-text">
+                <i class="fab fa-linkedin fa-4x"></i>
+              </a>
+              <a href="htttps://googleplus.com" target="_blank" class="white-text">
+                <i class="fab fa-google-plus fa-4x"></i>
+              </a>
+              <a href="htttps://pinterest.com" target="_blank" class="white-text">
+                <i class="fab fa-pinterest fa-4x"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    
+      <!-- Section: Gallery -->
+      <section id="gallery" class="section section-gallery scrollspy">
+        <div class="container">
+          <h4 class="center">
+            <span class="teal-text">Photo </span> Gallery
+          </h4>
+          <div class="row">
+            <div class="col s12 m3">
+              <img class="materialboxed responsive-img" src="https://source.unsplash.com/1600x900/?beach" alt="">
+            </div>
+            <div class="col s12 m3">
+              <img class="materialboxed responsive-img" src="https://source.unsplash.com/1600x900/?travel" alt="">
+            </div>
+            <div class="col s12 m3">
+              <img class="materialboxed responsive-img" src="https://source.unsplash.com/1600x900/?nature" alt="">
+            </div>
+            <div class="col s12 m3">
+              <img class="materialboxed responsive-img" src="https://source.unsplash.com/1600x900/?beach, travel" alt="">
+            </div>
+          </div>
+    
+          <div class="row">
+            <div class="col s12 m3">
+              <img class="materialboxed responsive-img" src="https://source.unsplash.com/1600x900/?beaches" alt="">
+            </div>
+            <div class="col s12 m3">
+              <img class="materialboxed responsive-img" src="https://source.unsplash.com/1600x900/?traveling" alt="">
+            </div>
+            <div class="col s12 m3">
+              <img class="materialboxed responsive-img" src="https://source.unsplash.com/1600x900/?bridge" alt="">
+            </div>
+            <div class="col s12 m3">
+              <img class="materialboxed responsive-img" src="https://source.unsplash.com/1600x900/?boat, travel" alt="">
+            </div>
+          </div>
+    
+          <div class="row">
+            <div class="col s12 m3">
+              <img class="materialboxed responsive-img" src="https://source.unsplash.com/1600x900/?water" alt="">
+            </div>
+            <div class="col s12 m3">
+              <img class="materialboxed responsive-img" src="https://source.unsplash.com/1600x900/?building" alt="">
+            </div>
+            <div class="col s12 m3">
+              <img class="materialboxed responsive-img" src="https://source.unsplash.com/1600x900/?trees" alt="">
+            </div>
+            <div class="col s12 m3">
+              <img class="materialboxed responsive-img" src="https://source.unsplash.com/1600x900/?cruise" alt="">
+            </div>
+          </div>
+        </div>
+      </section>
+    
+      <!-- Section: Contact -->
+      <section id="contact" class="section section-contact scrollspy">
+        <div class="container">
+          <div class="row">
+            <div class="col s12 m6">
+              <div class="card-panel teal white-text center">
+                <i class="material-icons medium">email</i>
+                <h5>Contact Us For Booking</h5>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus sed praesentium delectus. Sit, mollitia
+                  quo. Veniam repellat voluptas ipsum doloremque?</p>
+              </div>
+              <ul class="collection with-header">
+                <li class="collection-header">
+                  <h4>Location</h4>
+                </li>
+                <li class="collection-item">Travelville Agency</li>
+                <li class="collection-item">555 Beach rd, Suite 33</li>
+                <li class="collection-item">Miami FL, 55555</li>
+              </ul>
+            </div>
+            <div class="col s12 m6">
+              <div class="card-panel grey lighten-3">
+                <h5>Please fill out this form</h5>
+                <div class="input-field">
+                  <input type="text" placeholder="Name" id="name">
+                  <label for="name">Name</label>
+                </div>
+                <div class="input-field">
+                  <input type="email" placeholder="Email" id="email">
+                  <label for="email">Email</label>
+                </div>
+                <div class="input-field">
+                  <input type="text" placeholder="Phone" id="phone">
+                  <label for="phone">Phone</label>
+                </div>
+                <div class="input-field">
+                  <textarea class="materialize-textarea" placeholder="Enter Message" id="message"></textarea>
+                  <label for="message">Message</label>
+                </div>
+                <input type="submit" value="Submit" class="btn">
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    
+      <!-- Footer -->
+      <footer class="section teal darken-2 white-text center">
+        <p class="flow-text">Travelville &copy; 2018</p>
+      </footer>
 </body>
 </html>

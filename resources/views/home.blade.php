d2b338c2c4fae6ad266e53603a1759a2669fd84c
tintb03 <tinpdbh00128@fpt.edu.vn>
2023-07-27 13:52:49 +0700

    update home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,107 +1,203 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-    <title>Trang chủ</title>
-    <style>
-        .user-avatar {
-            width: 40px;
-            height: 40px;
-            border-radius: 50%;
-            background-color: #ccc;
-            display: inline-block;
-            cursor: pointer;
-        }
-
-        .user-menu {
-            display: none;
-            position: absolute;
-            top: 60px; /* Để bảng nhỏ hiển thị bên dưới avatar */
-            right: 0;
-            background-color: #f9f9f9;
-            padding: 8px;
-            border: 1px solid #ccc;
-        }
-
-        /* Thêm CSS để đảo ngược vị trí của phần hiển thị khi đăng nhập */
-        .user-info {
-            float: right;
-            margin-right: 20px;
-        }
-
-        .nav-links {
-            float: right;
-        }
-
-        /* Đặt kích thước của logo thành 200px */
-        .logo {
-            width: 200px;
-        }
-
-        /* CSS cho các nút đăng nhập và đăng ký */
-        .auth-button {
-            padding: 10px 20px;
-            background-color: #fff;
-            color: #00f;
-            border: 2px solid #00f;
-            border-radius: 5px;
-            text-decoration: none;
-            margin-left: 10px;
-        }
-
-        /* CSS cho màu chữ màu xanh */
-        .auth-button:hover {
-            background-color: #00f;
-            color: #fff;
-        }
-    </style>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@3.4.0/fonts/remixicon.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="{{ asset('css/style-home-page.css') }}">
+    <title>Booking</title>
 </head>
+
 <body>
-    <!-- Hiển thị logo với kích thước 200px -->
-    <img src="{{ asset('images/logo-booking-login.png') }}" alt="Booking.com Logo" class="logo">
-    
-    @if(session('success'))
-    <div>
-        <p style="color: green;">{{ session('success') }}</p>
+    <!-- Khung modal -->
+    <div class="modal-overlay" id="modal">
+        <div class="modal-content">
+            <span class="modal-close" onclick="closeModal()"><i class="ri-close-fill"></i></span>
+            <h2>{{ Auth::user()->name }}</h2>
+            <button onclick="logout()">Đăng xuất</button>
+            <button onclick="editProfile()">Chỉnh sửa profile</button>
+        </div>
     </div>
-    @endif
 
-    <!-- Hiển thị nút đăng nhập và đăng ký nếu người dùng chưa đăng nhập -->
-    @guest
-        <!-- Đảo ngược vị trí của các nút đăng nhập và đăng ký -->
-        <div class="nav-links">
-            <a href="/login" class="auth-button">Đăng nhập</a>
-            <a href="/register" class="auth-button">Đăng ký</a>
-        </div>
-    @else
-        <!-- Hiển thị avatar người dùng và thông tin người dùng -->
-        <div class="user-info">
-            <div class="user-avatar" id="userAvatar"></div>
-            
-            <!-- Hiển thị bảng nhỏ khi nhấp vào avatar -->
-            <div class="user-menu" id="userMenu">
-                <ul><span>{{ Auth::user()->name }}</span></ul>
-                <ul><a href="/user-profile">Xem thông tin tài khoản</a></ul>
-                <a href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="auth-button">Đăng xuất</a>
-                <form id="logout-form" action="{{ route('logout') }}" method="post" style="display: none;">
-                    @csrf
-                </form>
-            </div>
+    <script>
+        // JavaScript để điều khiển khung modal
+        document.addEventListener("DOMContentLoaded", function() {
+            const openModalBtn = document.querySelector(".user-icon a");
+            const modalOverlay = document.querySelector(".modal-overlay");
 
-            <!-- Script để xử lý sự kiện click vào avatar -->
-            <script>
-                var avatar = document.getElementById('userAvatar');
-                var menu = document.getElementById('userMenu');
+            function openModal() {
+                modalOverlay.style.display = "flex"; // Hiển thị khung modal
+            }
 
-                avatar.addEventListener('click', function() {
-                    if (menu.style.display === 'block') {
-                        menu.style.display = 'none';
+            function closeModal() {
+                modalOverlay.style.display = "none"; // Ẩn khung modal
+            }
+
+            function logout() {
+                fetch('/logout', {
+                    method: 'POST',
+                    headers: {
+                        'X-CSRF-TOKEN': '{{ csrf_token() }}',
+                    },
+                })
+                .then(response => {
+                    if (response.ok) {
+                        window.location.href = '/'; // Điều hướng về trang chủ sau khi đăng xuất
                     } else {
-                        menu.style.display = 'block';
+                        console.error('Có lỗi xảy ra khi đăng xuất');
                     }
+                })
+                .catch(error => {
+                    console.error('Có lỗi xảy ra khi gửi yêu cầu đăng xuất', error);
                 });
-            </script>
-        </div>
-    @endguest
+            }
 
+            openModalBtn.addEventListener("click", openModal);
+            modalOverlay.addEventListener("click", function(event) {
+                if (event.target === modalOverlay) {
+                    closeModal();
+                }
+            });
+        });
+    </script>
+
+    <nav>
+        <div class="nav__logo"><a href="/">Booking.Com</a></div>
+        <ul class="nav__links">
+            <li class="link"><a href="/">Home</a></li>
+            <li class="link"><a href="#">Book</a></li>
+            @auth
+                <li class="user-icon">
+                    <a href="javascript:void(0);"><i class="ri-account-pin-circle-fill"></i></a>
+                </li>
+            @else
+                <li class="link"><a href="/login">Login</a></li>
+                <li class="link"><a href="/register">Register</a></li>
+            @endauth
+        </ul>
+    </nav>
+    <header class="section__container header__container">
+        <div class="header__image__container">
+            <div class="header__content">
+                <h1>Enjoy Your Dream Vacation</h1>
+                <p>Book Hotels, Flights and stay packages at lowest price.</p>
+            </div>
+            <div class="booking__container">
+                <form>
+                    <div class="form__group">
+                        <div class="input__group">
+                            <input type="text" />
+                            <label>Location</label>
+                        </div>
+                        <p>Where are you going?</p>
+                    </div>
+                    <div class="form__group">
+                        <div class="input__group">
+                            <input type="text" />
+                            <label>Check In</label>
+                        </div>
+                        <p>Add date</p>
+                    </div>
+                    <div class="form__group">
+                        <div class="input__group">
+                            <input type="text" />
+                            <label>Check Out</label>
+                        </div>
+                        <p>Add date</p>
+                    </div>
+                    <div class="form__group">
+                        <div class="input__group">
+                            <input type="text" />
+                            <label>Room</label>
+                        </div>
+                        <p>Room Type</p>
+                    </div>
+                </form>
+                <button class="btn"><i class="ri-search-line"></i></button>
+            </div>
+        </div>
+    </header>
+
+    <section class="section__container popular__container">
+        <h2 class="section__header">Popular Hotels</h2>
+        <div class="popular__grid">
+            <div class="popular__card">
+                <img src="assets/hotel-1.jpg" alt="popular hotel" />
+                <div class="popular__content">
+                    <div class="popular__card__header">
+                        <h4>The Plaza Hotel</h4>
+                        <h4>$499</h4>
+                    </div>
+                    <p>New York City, USA</p>
+                </div>
+            </div>
+            <div class="popular__card">
+                <img src="assets/hotel-2.jpg" alt="popular hotel" />
+                <div class="popular__content">
+                    <div class="popular__card__header">
+                        <h4>Ritz Paris</h4>
+                        <h4>$549</h4>
+                    </div>
+                    <p>Paris, France</p>
+                </div>
+            </div>
+            <!-- ... Các phần tử của popular hotels khác ... -->
+        </div>
+    </section>
+
+    <section class="client">
+        <div class="section__container client__container">
+            <h2 class="section__header">What our client say</h2>
+            <div class="client__grid">
+                <div class="client__card">
+                    <img src="assets/client-1.jpg" alt="client" />
+                    <p>
+                        The booking process was seamless, and the confirmation was
+                        instant. I highly recommend WDM&amp;Co for hassle-free hotel bookings.
+                    </p>
+                </div>
+                <!-- ... Các phần tử client cards khác ... -->
+            </div>
+        </div>
+    </section>
+
+    <section class="section__container">
+        <div class="reward__container">
+            <p>100+ discount codes</p>
+            <h4>Join rewards and discover amazing discounts on your booking</h4>
+            <button class="reward__btn">Join Rewards</button>
+        </div>
+    </section>
+
+    <footer class="footer">
+        <div class="section__container footer__container">
+            <div class="footer__col">
+                <h3>WDM&amp;Co</h3>
+                <p>
+                    WDM&amp;Co is a premier hotel booking website that offers a seamless and
+                    convenient way to find and book accommodations worldwide.
+                </p>
+                <p>
+                    With a user-friendly interface and a vast selection of hotels,
+                    WDM&amp;Co aims to provide a stress-free experience for travelers
+                    seeking the perfect stay.
+                </p>
+            </div>
+            <div class="footer__col">
+                <h4>Company</h4>
+                <p>About Us</p>
+                <p>Our Team</p>
+                <p>Blog</p>
+                <p>Book</p>
+                <p>Contact Us</p>
+            </div>
+            <!-- ...foote ... -->
+        </div>
+        <div class="footer__bar">
+            Copyright © 2023 Web Design Mastery. All rights reserved.
+        </div>
+    </footer>
 </body>
+
 </html>
